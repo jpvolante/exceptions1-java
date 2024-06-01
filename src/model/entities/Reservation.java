69b64fbacc73c74f0efb92e7f513c25d0a593a7d
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-06-01 10:07:11 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -39,9 +39,18 @@ public class Reservation {
 
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){
+        Date now = new Date();
+        if(checkIn.before(now) || checkOut.before(now)){
+            return "Error in reservation: Check-out date must be future dates";
+        }
+        if(!checkOut.after(checkIn)){
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null; //caso não tenha nenhum erro
     }
 
     @Override
